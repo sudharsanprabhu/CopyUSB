558aadb5c66193425dd9ca923bedde0c48829d0e
sudharsanprabhu <sudharsanprabhu@users.noreply.github.com>
2020-09-23 18:07:09 +0530

    Initial commit

---
+++ CopyUSB/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CopyUSB
+{
+    partial class CopyUSB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CopyUSB));
+            this.label = new System.Windows.Forms.Label();
+            this.location = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.hideBtn = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label.Location = new System.Drawing.Point(6, 29);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(126, 24);
+            this.label.TabIndex = 0;
+            this.label.Text = "Output Folder";
+            // 
+            // location
+            // 
+            this.location.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.location.Enabled = false;
+            this.location.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.location.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.location.Location = new System.Drawing.Point(10, 71);
+            this.location.Name = "location";
+            this.location.ReadOnly = true;
+            this.location.Size = new System.Drawing.Size(243, 22);
+            this.location.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(268, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hideBtn
+            // 
+            this.hideBtn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.hideBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hideBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.hideBtn.Location = new System.Drawing.Point(10, 114);
+            this.hideBtn.Name = "hideBtn";
+            this.hideBtn.Size = new System.Drawing.Size(155, 35);
+            this.hideBtn.TabIndex = 3;
+            this.hideBtn.Text = "Hide";
+            this.hideBtn.UseVisualStyleBackColor = false;
+            this.hideBtn.Click += new System.EventHandler(this.hideBtn_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "CopyUSB";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CopyUSB";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.hideBtn);
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.location);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 180);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(191, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(152, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Show in System Tray";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // CopyUSB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(386, 208);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CopyUSB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CopyUSB [Prometheus Hephaesteus]";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox location;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button hideBtn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ CopyUSB/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyUSB
+{
+    public partial class CopyUSB : Form
+    {
+        public CopyUSB()
+        {
+            InitializeComponent();
+           
+        }
+
+         String usb = "";
+         String destination = "";
+         bool isDestinaionSet=false;
+
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int WM_DEVICEARRIVAL = 0x8000;
+        private const int WM_DEVICEREMOVED = 0x8004;
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                location.Text = fbd.SelectedPath;
+                
+                var time = DateTime.Now;
+                destination = Path.Combine(fbd.SelectedPath, "CopyUSB "+time.ToString("dd-MM-yyyy_HH-mm-ss"));
+                
+                Directory.CreateDirectory(destination);
+                isDestinaionSet = true;
+                
+            }        
+           
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (isDestinaionSet)
+            {
+                if ((int)m.WParam == WM_DEVICEARRIVAL)
+                    setSource();
+                else if ((int)m.WParam == WM_DEVICEREMOVED)
+                    Application.Exit();
+            }
+        }
+
+
+        private void setSource()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    usb = drive.ToString();
+                    copy(usb, destination);
+
+                }
+            }
+        }
+
+
+        private void copy(string source, string destination)
+        {
+            
+            
+
+            String filename = "";
+            String target = "";
+
+            if (!Directory.Exists(destination))
+                Directory.CreateDirectory(destination);
+
+                 String[] files = Directory.GetFiles(source);
+            foreach (String file in files)
+            {
+                filename = Path.GetFileName(file);
+               
+                    target = Path.Combine(destination, filename);
+                    File.Copy(file, target);
+                
+            }
+
+            String[] folders = Directory.GetDirectories(source);
+            foreach(String folder in folders)
+            {
+                filename = Path.GetFileName(folder);
+                if (filename != "System Volume Information")
+                {
+                    target = Path.Combine(destination, filename);
+                    copy(folder, target);
+                }
+            }
+           
+            }
+
+        private void hideBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            if (checkBox1.Checked == false)
+                notifyIcon1.Visible = false;
+                   
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void pwdText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
